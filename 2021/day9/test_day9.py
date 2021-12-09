bb0fe8437c250a65b07d29575b7c189052bf4394
KarlT <karl@tillstromengineering.se>
2021-12-09 18:27:33 +0100

    day 9: experiment with better solution for part2

--- 2021/day9/test_day9.py
+++ 2021/day9/test_day9.py
@@ -1,6 +1,73 @@
 import math
 import sys
 import unittest
+from collections import deque
+import timeit
+
+
+def better_part_2(part):
+    # https://github.com/jonathanpaulson/AdventOfCode/blob/master/2021/9.py
+
+    filename = "input.txt"
+
+    field = []
+    with open(filename) as openfileobject:
+        for line in openfileobject:
+            tline = line.strip()
+            if tline:
+                field.append([int(c) for c in tline])
+
+    low_values = []
+    low_points = []
+
+    max_col = len(field[0])
+    max_row = len(field)
+
+    DR = [-1, 0, 1, 0]
+    DC = [0, 1, 0, -1]
+
+    for row in range(max_row):
+        for col in range(max_col):
+            current_val = field[row][col]
+
+            is_low_point = True
+            for direction in range(4):
+                r = row + DR[direction]
+                c = col + DC[direction]
+
+                if 0 <= r < max_row and 0 <= c < max_col and field[r][c] <= field[row][col]:
+                    is_low_point = False
+                    break
+
+            if is_low_point:
+                low_values.append(current_val)
+                low_points.append((row, col))
+
+    if part == 1:
+        return sum(low_values) + len(low_values)
+
+    basins = []
+
+    for basin in low_points:
+        q = deque()
+        q.append(basin)
+        size = 0
+        while q:
+            row, col = q.popleft()
+            if field[row][col] == 9:
+                continue
+
+            field[row][col] = 9
+
+            size += 1
+            for direction in range(4):
+                r = row + DR[direction]
+                c = col + DC[direction]
+                if 0 <= r < max_row and 0 <= c < max_col and field[r][c] != 9:
+                    q.append((r, c))
+        basins.append(size)
+
+    return math.prod(sorted(basins, reverse=True)[0:3])
 
 
 def solve(part, useExample):
@@ -70,10 +137,14 @@ def solve(part, useExample):
 
     largest = sorted([len(x) for x in low_points], reverse=True)[0:3]
 
-
     return math.prod(largest)
 
-print(solve(2, False))
+
+print("mine")
+print(timeit.timeit("solve(2, False)", number=10, globals=globals()))
+print("better")
+print(timeit.timeit("better_part_2(2)", number=10, globals=globals()))
+
 
 
 class AocTest(unittest.TestCase):

2843a8e807800f556c8b025d077c0ed2b0b88199
KarlT <karl@tillstromengineering.se>
2021-12-10 06:40:36 +0100

    Add values to tests

--- 2021/day10/test_day10.py
+++ 2021/day10/test_day10.py
@@ -70,13 +70,13 @@ print(solve(2, False))
 
 class AocTest(unittest.TestCase):
     def test_part_a_real(self):
-        self.assertEqual(4421, solve(1, False), "Part 1 REAL")
+        self.assertEqual(369105, solve(1, False), "Part 1 REAL")
 
     def test_part_a_example(self):
-        self.assertEqual(5, solve(1, True), "Part 1 example")
+        self.assertEqual(26397, solve(1, True), "Part 1 example")
 
     def test_part_b_real(self):
-        self.assertEqual(18674, solve(2, False), "Part 2 REAL")
+        self.assertEqual(3999363569, solve(2, False), "Part 2 REAL")
 
     def test_part_b_example(self):
-        self.assertEqual(12, solve(2, True), "Part 2 example")
+        self.assertEqual(288957, solve(2, True), "Part 2 example")

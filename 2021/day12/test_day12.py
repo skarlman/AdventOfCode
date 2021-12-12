5e7f7144321e423a2606e7a31128a6c1f03c3f58
KarlT <karl@tillstromengineering.se>
2021-12-12 18:56:39 +0100

    Day 12 some cleanup

--- 2021/day12/test_day12.py
+++ 2021/day12/test_day12.py
@@ -1,84 +1,73 @@
 import unittest
+from collections import defaultdict
 
-print()
-print('--- new run ---')
-print()
 
-def explore(found_paths, nog_go, twice_ok, all_paths):
-    resuling_paths = []
+def explore(explored_paths, no_go, twice_is_ok, all_paths):
+    resulting_path = []
 
-    for current_path in found_paths:
+    for current_path in explored_paths:
 
-        previousCave = current_path[-1]
-        if previousCave == 'end':
-            resuling_paths.append(current_path)
-            continue
-        for possible in all_paths[previousCave]:
+        previous_cave = current_path[-1]
+        if previous_cave == 'end':
+            resulting_path.append(current_path)
+            return resulting_path
+
+        for possible in all_paths[previous_cave]:
             if possible == 'start':
                 continue
 
-            if (previousCave, possible) in nog_go:
+            # (unnecessary) guard against infinite loop if two capital caves are connected
+            if (previous_cave, possible) in no_go:
                 continue
-
-            next_twice_ok = twice_ok
-            if ord(possible[0]) >= 97 and current_path.count(possible) >= 1:
-                if twice_ok:
-                    next_twice_ok = False
-                else:
-                    continue
-
-
 
             new_path = list(current_path)
             new_path.append(possible)
 
-            new_no_go = nog_go.copy()
+            new_no_go = no_go.copy()
 
-            if twice_ok:
-                if ord(possible[0]) < 97 and ord(previousCave[0]) < 97:
-                    new_no_go.add((previousCave, possible))
+            if twice_is_ok:
+                if ord(possible[0]) < 97 and ord(previous_cave[0]) < 97:
+                    new_no_go.add((previous_cave, possible))
             else:
-                new_no_go.add((previousCave, possible))
+                new_no_go.add((previous_cave, possible))
 
-            new_found = explore([new_path], new_no_go, next_twice_ok, all_paths)
-            resuling_paths.extend(new_found)
+            next_twice_is_ok = twice_is_ok
+            if ord(possible[0]) >= 97 and current_path.count(possible) >= 1:
+                if twice_is_ok:
+                    next_twice_is_ok = False
+                else:
+                    continue
 
-    return resuling_paths
+            new_found_paths = explore([new_path], new_no_go, next_twice_is_ok, all_paths)
+            resulting_path.extend(new_found_paths)
+
+    return resulting_path
 
 
 def solve(part, useExample):
     filename = "exampleinput.txt" if useExample else "input.txt"
-    all_paths = dict()
+    all_paths = defaultdict(set)
 
     with open(filename) as openfileobject:
         for line in openfileobject:
             parts = line.strip().split('-')
 
-            if parts[0] not in all_paths:
-                all_paths[parts[0]] = set()
-
             if parts[1] not in all_paths[parts[0]]:
                 all_paths[parts[0]].add(parts[1])
-
-            if parts[1] not in all_paths:
-                all_paths[parts[1]] = set()
 
             if parts[0] not in all_paths[parts[1]]:
                 all_paths[parts[1]].add(parts[0])
 
+    found_paths = explore([['start']], set(), part == 2, all_paths)
 
-
-    no_go = set()
-    no_go.add(('start', '-1'))
-    found_paths = explore([['start']], no_go, part==2, all_paths)
-
-#    for p in found_paths:
-#        print("|".join(p))
+    #    for p in found_paths:
+    #        print("|".join(p))
 
     return len(found_paths)
 
 
-print(solve(2, True))
+print(f'Part 1: {solve(1, False)}')
+print(f'Part 2: {solve(2, False)}')
 
 
 class AocTest(unittest.TestCase):

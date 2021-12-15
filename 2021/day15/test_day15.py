e1d8d8b474817daa35c41e693e8f7f89821c6c2c
karlt <karl.tillstrom@gmail.com>
2021-12-15 11:55:28 +0100

    Day 15 working with dijkstra instead

--- 2021/day15/test_day15.py
+++ 2021/day15/test_day15.py
@@ -1,15 +1,16 @@
+import heapq
 import sys
 import unittest
 from collections import defaultdict, deque
 
-
-l =[]
-for i in range(998,1015):
-    n = i%9 + 1
+l = []
+for i in range(998, 1015):
+    n = i % 9 + 1
     l.append(n)
 
-#print(l)
-#exit(0)
+
+# print(l)
+# exit(0)
 
 def solve(part, useExample):
     filename = "exampleinput.txt" if useExample else "input.txt"
@@ -22,50 +23,61 @@ def solve(part, useExample):
     max_row = len(G)
     max_col = len(G[0])
 
-    C = [[0 for x in range(max_col*5)] for x in range(max_row*5)]
-    G2 = [[0 for x in range(max_col*5)] for x in range(max_row*5)]
-
+    C = [[sys.maxsize for x in range(max_col * 5)] for x in range(max_row * 5)]
+    G2 = [[0 for x in range(max_col * 5)] for x in range(max_row * 5)]
 
     for extra_row in range(5):
         for extra_col in range(5):
             for row in range(max_row):
                 for col in range(max_col):
-                    c_row = row + extra_row*max_row
-                    c_col = col + extra_col*max_col
-#                    print(f'R:{row},{col}  C:{c_row},{c_col}')
+                    c_row = row + extra_row * max_row
+                    c_col = col + extra_col * max_col
 
                     if c_row == 0 and c_col == 0:
                         continue
 
-                    costs =[]
-
-
-                    new_g = ((G[row][col] + extra_row + extra_col)-1) % 9 + 1
-
-                    if row==99 and col==99:
-                        print(new_g, end='')
-
+                    new_g = ((G[row][col] + extra_row + extra_col) - 1) % 9 + 1
                     G2[c_row][c_col] = new_g
-                    upper = new_g
-                    if c_row > 0:
-                        costs.append(upper + C[c_row - 1][c_col])
 
-                    lefter = new_g
-                    if c_col > 0:
-                        costs.append(lefter + C[c_row][c_col-1])
+    # pathfinding!
+    frontier = []
+    c_r = 0
+    c_c = 0
 
-                    C[c_row][c_col] = min(costs)
-        print()
+    heapq.heappush(frontier, (0, (c_r, c_c)))
+    C[c_r][c_c] = 0
+
+    DR = [-1, 0, 1, 0]
+    DC = [0, 1, 0, -1]
+
+    while frontier:
+        priority, (row, col) = heapq.heappop(frontier)
+
+        if row == (max_row*5) - 1 and col == (max_col*5) - 1:
+            C[(max_row*5)-1][(max_col*5)-1] = priority
+            break
+
+        neighbors = []
+        for dirs in range(4):
+            rr = row + DR[dirs]
+            cc = col + DC[dirs]
+            if 0<= rr < (max_row*5) and 0<=cc<(max_col*5):
+                neighbors.append((rr,cc))
+
+        for (nr,nc) in neighbors:
+            cost=C[row][col] + G2[nr][nc]
+            if cost < C[nr][nc]:
+                C[nr][nc] = cost
+                heapq.heappush(frontier, (cost, (nr,nc)))
 
 
-    for r in C[0:3]:
-        print(",".join([str(i) for i in r]))
-        print()
+
     return C[-1][-1]
 
     return None
 
-#gues 2944 part 2 (too high)
+
+# gues 2944 part 2 (too high)
 print(solve(1, False))
 
 

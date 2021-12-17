28582b85f3335afc2b6a48b167c6530dde8ce422
KarlT <karl@tillstromengineering.se>
2021-12-17 10:36:14 +0100

    Prepare Day 18

---
+++ 2021/day18/test_day18.py
@@ -0,0 +1,31 @@
+import unittest
+from collections import defaultdict, deque
+
+def solve(part, useExample):
+    filename = "exampleinput.txt" if useExample else "input.txt"
+
+    # TODO put solution here
+    with open(filename) as openfileobject:
+        for line in openfileobject:
+            tline = line.strip()
+
+            pass
+
+    return None
+
+
+print(solve(1, True))
+
+
+class AocTest(unittest.TestCase):
+    def test_part_a_real(self):
+        self.assertEqual(4421, solve(1, False), "Part 1 REAL")
+
+    def test_part_a_example(self):
+        self.assertEqual(5, solve(1, True), "Part 1 example")
+
+    def test_part_b_real(self):
+        self.assertEqual(18674, solve(2, False), "Part 2 REAL")
+
+    def test_part_b_example(self):
+        self.assertEqual(12, solve(2, True), "Part 2 example")

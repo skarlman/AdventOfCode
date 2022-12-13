595fe9e77c96b4b3fec67ab2ac62736a6a609c59
karlt <karl.tillstrom@gmail.com>
2022-12-13 09:24:12 +0100

    day 13 wip

--- 2022/day13/test_day13.py
+++ 2022/day13/test_day13.py
@@ -1,44 +1,5 @@
+import json
 from collections import defaultdict, deque
-
-
-def find_closing_bracket(s):
-    opening_brackets = 0
-    for i, c in enumerate(s):
-        if c == '[':
-            opening_brackets += 1
-        elif c == "]":
-            opening_brackets -= 1
-            if opening_brackets == 0:
-                return i
-
-    return -1
-
-
-def splitup(s):
-    # [1],[2,3,4]
-    if len(s) == 0:
-        return []
-
-    if s[0] == ',':
-        s = s[1:]
-
-    comma = s.find(',')
-    bracket = s.find('[')
-
-    if -1 < comma < bracket:
-        parts = [s[:comma]] + splitup(s[comma:])
-    elif -1 < bracket < comma:
-        closing_bracket = find_closing_bracket(s)
-        parts = splitup(s[bracket + 1:closing_bracket])
-
-        if closing_bracket < len(s) -1:
-            parts += [splitup(s[closing_bracket + 1:])]
-    else:
-        parts = s.split(',')
-        if len(parts) == 1:
-            parts = [parts]
-
-    return parts
 
 
 def solve(part, use_example):
@@ -46,17 +7,38 @@ def solve(part, use_example):
 
     score_part1 = 0
     with open(filename) as openfileobject:
-        # raw = openfileobject.read()
-        lines = openfileobject.readlines()
+        raw = openfileobject.read()
+        #lines = openfileobject.readlines()
 
-#    s = "[[1],[2,3,4]]"
-    s="[1,[2,[3,[4,[5,6,7]]]],8,9]"
-    s = splitup(s[1:-1])
+    chunks = raw.split('\n\n')
 
-    print(s)
+#If both values are integers, the lower integer should come first.
+    # If the left integer is lower than the right integer, the inputs are in the right order.
+    # If the left integer is higher than the right integer, the inputs are not in the right order.
+    # Otherwise, the inputs are the same integer; continue checking the next part of the input.
+
+# If both values are lists, compare the first value of each list, then the second value, and so on.
+    # If the left list runs out of items first, the inputs are in the right order.
+    # If the right list runs out of items first, the inputs are not in the right order.
+    # If the lists are the same length and no comparison makes a decision about the order, continue checking the next part of the input.
+
+# If exactly one value is an integer, convert the integer to a list which contains that integer as its only value,
+    # then retry the comparison.
+    # For example, if comparing [0,0,0] and 2, convert the right value to [2] (a list containing 2);
+    # the result is then found by instead comparing [0,0,0] and [2].
+
+
+    for chunk in chunks:
+        rows = [json.loads(r.strip()) for r in chunk.split()]
+
+
+        print()
+        print(rows[0])
+        print(rows[1])
+
 
     return score_part1
 
 
-print(f'Part 1: {solve(1, False)}')
-print(f'Part 2: {solve(2, False)}')
+print(f'Part 1: {solve(1, True)}')
+# print(f'Part 2: {solve(2, False)}')
